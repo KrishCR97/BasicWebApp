7fca16066e19243f2e8e0790f2474f15abfa0437
Veera Kishon Kumar Mucherla <veerakishon.mucherla@gmail.com>
2018-06-08 12:41:01 -0400

    Added code to get the forum data.

--- src/com/cr7/lifecycle/LifeCycle.java
+++ src/com/cr7/lifecycle/LifeCycle.java
@@ -1,6 +1,9 @@
 package com.cr7.lifecycle;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,18 +15,19 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class LifeCycle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LifeCycle() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LifeCycle() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
+	@Override
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
 	}
@@ -31,24 +35,41 @@ public class LifeCycle extends HttpServlet {
 	/**
 	 * @see Servlet#destroy()
 	 */
+	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		int deptNumber = Integer.parseInt(request.getParameter("deptNumber"));
+		String deptName = request.getParameter("deptName");
+		String deptLocation = request.getParameter("deptLocation");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<hr><hr/>");
+		out.println("<p> Department Name : " + deptName);
+		out.println("</p>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
